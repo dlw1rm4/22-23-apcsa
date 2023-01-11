52ca4ad614d40aa08ba5b0e578a4a7fe26593d6b
dlw1rm4 <118654100+dlw1rm4@users.noreply.github.com>
2023-01-11 13:38:39 -0800

    Add files via upload

---
+++ MathFun.java
@@ -0,0 +1,20 @@
+/**
+ * Description: Calculate the area and perimeter of an irregular polygon given the coordinate points, then drawing it out
+ * Purpose: To learn about array lists
+ * @author (Kaitlyn Nguy)
+ * @version (12.16.22)
+ */
+import java.awt.geom.*;
+import java.util.*;
+import apcslib.*;
+import chn.util.*;
+public class MathFun
+{
+    public static void main(String args[]) {
+        int intAnswer1 = 4 + 9;
+        System.out.println("4 + 9 = " + intAnswer1);
+        int intAnswer2 = (int)7.75 + 2;
+        System.out.println("7.75 + 2 = " + intAnswer2);
+        System.out.println("The largest value of type int = " + Integer.MAX_VALUE);
+    }
+}
